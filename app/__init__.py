6920e61a15e366d9f3f3887e7063106a07970ca1
leskov-m <leskov760@gmail.com>
2025-12-24 01:46:10 +0500

    feat: flask application

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,18 +8,20 @@ from app.config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS
 db = SQLAlchemy()
 
 
-def create_app():
+def create_app(conf: Optional[dict] = None):
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
+    conf = conf or {}
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = conf.get('SQLALCHEMY_DATABASE_URI', SQLALCHEMY_DATABASE_URI)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = conf.get(
+        'SQLALCHEMY_TRACK_MODIFICATIONS',
+        SQLALCHEMY_TRACK_MODIFICATIONS
+    )
 
     db.init_app(app)
 
     from app.routes import posts_bp
     app.register_blueprint(posts_bp)
-
-    # with app.app_context():
-    #     db.create_all()
 
     return app
